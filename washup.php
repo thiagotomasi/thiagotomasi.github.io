4e6b55779c0b11918cd3c0da9900dc3532edb93e
Thiago Tomasi <alpaca.thiago@gmail.com>
2016-05-29 13:43:44 +0300

    replace all php files to HTML files

--- washup.php
+++ washup.php
@@ -1,13 +1,23 @@
 <!DOCTYPE HTML>
 <html>
 	<head>
-		<title>Washup - Thiago Tomasi | UX/UI Designer and Front-end Engineer</title>
-		<?php include("head.inc.php"); ?>
+		<title>Thiago Tomasi | UX/UI Designer and Front-end Engineer</title>
+		<meta charset="utf-8" />
+		<meta name="author" content="Thiago Tomasi" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="assets/css/main.min.css" />
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.min.css" /><![endif]-->
 	</head>
 	<body id="top">
 
 		<!-- Header -->
-			<?php include("header.inc.php"); ?>
+			<header id="header">
+				<a href="http://thiagottomasi.com/portfolio/" class="image avatar"><img src="images/avatar.jpg?123" alt="Thiago Tomasi photo profile" /></a>
+				<h1><strong>Thiago Tomasi</strong><br>
+				UX/UI Designer | Front-end Engineer</h2>
+				<p><a href="http://thiagottomasi.com/portfolio/" class="button special small">View all projects</a></p>
+			</header>
 
 		<!-- Main -->
 			<div id="main">
@@ -42,10 +52,23 @@
 			</div>
 
 		<!-- Footer -->
-			<?php include("footer.inc.php"); ?>
+			<footer id="footer">
+				<ul class="icons">
+					<li><a href="mailto:alpaca.thiago@gmail.com" class="icon fa-envelope-o"><span class="label">Email</span></a></li>
+					<li><a target="_blank" href="https://twitter.com/thiagotomasi" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+					<li><a target="_blank" href="https://www.facebook.com/thiagotomasi" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+					<li><a target="_blank" href="https://github.com/thiagotomasi" class="icon fa-github"><span class="label">Github</span></a></li>
+					<li><a target="_blank" href="https://medium.com/@thiagotomasi" class="icon fa-medium"><span class="label">Medium</span></a></li>
+					<li><a target="_blank" href="https://br.linkedin.com/in/thiagotomasi" class="icon fa-linkedin"><span class="label">Linkedin</span></a></li>
+				</ul>
+			</footer>
 
 		<!-- Scripts -->
-			<?php include("foot.inc.php"); ?>
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.poptrox.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="assets/js/main.min.js"></script>
 			
 	</body>
 </html>
