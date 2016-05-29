0947c5dcba43a34ea73d2e4046fe1b8a31bb2a8d
Thiago Tomasi <alpaca.thiago@gmail.com>
2016-05-29 13:46:03 +0300

    replace extensions to HTML

--- foot.inc.php
+++
@@ -1,5 +0,0 @@
-<script src="assets/js/jquery.min.js"></script>
-<script src="assets/js/jquery.poptrox.min.js"></script>
-<script src="assets/js/skel.min.js"></script>
-<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
-<script src="assets/js/main.min.js"></script>
